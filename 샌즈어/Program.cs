1b75482d44b31dd4a0f1d3af3b08cc9d766f33b0
asdf-2345 <kiaaa741@gmail.com>
2020-02-15 14:29:53 +0900

    README.md 업로드

--- 샌즈어/Program.cs
+++ 샌즈어/Program.cs
@@ -11,58 +11,10 @@ using System.IO;
 using System.Collections;
 namespace 샌즈어
 {
-	//출력
-	//  참고로("출력할 내용")
-	//  참고로("출력할 내용")습니다  (다음줄로 넘어감)
-	//	참고로('변수이름')
-	//	참고로('변수이름')
-	
-	//변수
-	//	정수형 - 샌즈의 공격은 '변수이름'당 '정수'이 답니다
-	//	문자열 - 샌즈는 '문자열'한 '변수이름'공격을 가지고 있습니다
-	
-	//만들예정인거
-	
-	//조건문
-	//	하지만 잠이 들었을때 창을 옮겨서 (변수1,변수2)을 시도하고 1차공격은
-	//	{
-	//		참고로("피하지만")습니다
-	//	}
-	//조건문안에 넣은 변수2개가 같은지만 비교해줍니다.
-	
-	//반복문
-	//	자신의 턴을 '정수'동안 유지한채로 잠에듭니다
-	//	[
-	//		참고로("겁나 어렵습니다.")
-	//	]
-	//
-	//예
-	//	자신의 턴을 3동안 유지한채로 잠에듭니다
-	//	[
-	//		참고로("겁나 어렵습니다.")
-	//	]
-	//출력 : 겁나 어렵습니다겁나 어렵습니다겁나 어렵습니다
-
 	class Program
 	{
-		//string[] lines;
+		static string[] lines;
 		
-		//테스트용
-		static string[] lines = {"참고로(\"피하지만\")습니다",
-								 "참고로(\"샌즈 아시는구나\")습니다",
-								 "참고로(\"샌즈 아시는구나\")습니다",
-								 "샌즈는 123한 테스트공격을 가지고 있습니다",
-								 "샌즈의 공격은 테스트2당 123이 답니다",
-								 "참고로(테스트)습니다",
-								 "참고로(테스트2)습니다",
-								 "하지만 잠이 들었을때 창을 옮겨서 (테스트,테스트2)을 시도하고 1차공격은",
-								 "{",
-								 "참고로(\"샌즈 아시는구나\")습니다",
-								 "}",
-								 "자신의 턴을 3동안 유지한채로 잠에듭니다",
-								 "[",
-								 "참고로(\"반복중\")습니다",
-								 "]"};
 		public static Hashtable intHt = new Hashtable();
 		public static Hashtable strHt = new Hashtable();
 		public static int numberOfRepetitions = 0;
@@ -71,7 +23,7 @@ namespace 샌즈어
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("실행시킬 파일 위치를 입력해주세요.");
-			//lines = File.ReadAllLines(Console.ReadLine());
+			lines = File.ReadAllLines(Console.ReadLine());
 			for(int a = 0; a < lines.Length; a++){
 				if(lines[a].Contains("참고로")){
 					bool linebreak = false;
